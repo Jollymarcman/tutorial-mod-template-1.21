7e923376d3d80253e37904393c776656a0fafc97
Jollymarcman <jollymarcman2446@gmail.com>
2025-04-29 19:17:32 +1000

    added eggmerciumingot

--- src/main/java/net/marcuscampbell/tutorialmod/TutorialMod.java
+++ src/main/java/net/marcuscampbell/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.marcuscampbell.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.marcuscampbell.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,5 +12,7 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+
+		ModItems.registerModItems();
 	}
 }
